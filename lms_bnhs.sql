6a4037a2895d2186893a9d2250293e4c40ad6bcd
Marky <mansuetomarky@gmail.com>
2026-02-26 00:28:12 +0800

    Update the Database

---
+++ lms_bnhs.sql
@@ -0,0 +1,283 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 25, 2026 at 05:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms_bnhs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_tokens`
+--
+-- Error reading structure for table lms_bnhs.api_tokens: #1932 - Table &#039;lms_bnhs.api_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.api_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`api_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignments`
+--
+-- Error reading structure for table lms_bnhs.assignments: #1932 - Table &#039;lms_bnhs.assignments&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.assignments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`assignments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment_submissions`
+--
+-- Error reading structure for table lms_bnhs.assignment_submissions: #1932 - Table &#039;lms_bnhs.assignment_submissions&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.assignment_submissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`assignment_submissions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance_records`
+--
+-- Error reading structure for table lms_bnhs.attendance_records: #1932 - Table &#039;lms_bnhs.attendance_records&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.attendance_records: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`attendance_records`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+-- Error reading structure for table lms_bnhs.cache: #1932 - Table &#039;lms_bnhs.cache&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.cache: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`cache`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+-- Error reading structure for table lms_bnhs.cache_locks: #1932 - Table &#039;lms_bnhs.cache_locks&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.cache_locks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`cache_locks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+-- Error reading structure for table lms_bnhs.courses: #1932 - Table &#039;lms_bnhs.courses&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.courses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`courses`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_materials`
+--
+-- Error reading structure for table lms_bnhs.course_materials: #1932 - Table &#039;lms_bnhs.course_materials&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.course_materials: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`course_materials`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+-- Error reading structure for table lms_bnhs.enrollments: #1932 - Table &#039;lms_bnhs.enrollments&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.enrollments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`enrollments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table lms_bnhs.failed_jobs: #1932 - Table &#039;lms_bnhs.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade_entries`
+--
+-- Error reading structure for table lms_bnhs.grade_entries: #1932 - Table &#039;lms_bnhs.grade_entries&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.grade_entries: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`grade_entries`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guardians`
+--
+-- Error reading structure for table lms_bnhs.guardians: #1932 - Table &#039;lms_bnhs.guardians&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.guardians: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`guardians`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guardian_students`
+--
+-- Error reading structure for table lms_bnhs.guardian_students: #1932 - Table &#039;lms_bnhs.guardian_students&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.guardian_students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`guardian_students`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+-- Error reading structure for table lms_bnhs.jobs: #1932 - Table &#039;lms_bnhs.jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+-- Error reading structure for table lms_bnhs.job_batches: #1932 - Table &#039;lms_bnhs.job_batches&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.job_batches: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`job_batches`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table lms_bnhs.migrations: #1932 - Table &#039;lms_bnhs.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+-- Error reading structure for table lms_bnhs.notifications: #1932 - Table &#039;lms_bnhs.notifications&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.notifications: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`notifications`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table lms_bnhs.password_reset_tokens: #1932 - Table &#039;lms_bnhs.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_cards`
+--
+-- Error reading structure for table lms_bnhs.report_cards: #1932 - Table &#039;lms_bnhs.report_cards&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.report_cards: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`report_cards`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_card_items`
+--
+-- Error reading structure for table lms_bnhs.report_card_items: #1932 - Table &#039;lms_bnhs.report_card_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.report_card_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`report_card_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+-- Error reading structure for table lms_bnhs.roles: #1932 - Table &#039;lms_bnhs.roles&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`roles`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school_years`
+--
+-- Error reading structure for table lms_bnhs.school_years: #1932 - Table &#039;lms_bnhs.school_years&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.school_years: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`school_years`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+-- Error reading structure for table lms_bnhs.sections: #1932 - Table &#039;lms_bnhs.sections&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.sections: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`sections`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sms_logs`
+--
+-- Error reading structure for table lms_bnhs.sms_logs: #1932 - Table &#039;lms_bnhs.sms_logs&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.sms_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`sms_logs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+-- Error reading structure for table lms_bnhs.students: #1932 - Table &#039;lms_bnhs.students&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`students`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+-- Error reading structure for table lms_bnhs.subjects: #1932 - Table &#039;lms_bnhs.subjects&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.subjects: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`subjects`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_assignments`
+--
+-- Error reading structure for table lms_bnhs.subject_assignments: #1932 - Table &#039;lms_bnhs.subject_assignments&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.subject_assignments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`subject_assignments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_final_grades`
+--
+-- Error reading structure for table lms_bnhs.subject_final_grades: #1932 - Table &#039;lms_bnhs.subject_final_grades&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.subject_final_grades: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`subject_final_grades`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sync_batches`
+--
+-- Error reading structure for table lms_bnhs.sync_batches: #1932 - Table &#039;lms_bnhs.sync_batches&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.sync_batches: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`sync_batches`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+-- Error reading structure for table lms_bnhs.teachers: #1932 - Table &#039;lms_bnhs.teachers&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.teachers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`teachers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table lms_bnhs.users: #1932 - Table &#039;lms_bnhs.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+-- Error reading structure for table lms_bnhs.user_roles: #1932 - Table &#039;lms_bnhs.user_roles&#039; doesn&#039;t exist in engine
+-- Error reading data for table lms_bnhs.user_roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lms_bnhs`.`user_roles`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
